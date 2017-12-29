904e4fb1a7e412f94eb33cb1b0978197b888d65d
Joanna <joan-a@o2.pl>
2017-12-30 00:30:45 +0100

    Wyswietlam jak cos jest

--- Hejka.java
+++ Hejka.java
@@ -1,16 +1,18 @@
 import java.util.Arrays;
 
-public class Hejka{
+public class Hejka {
 
 
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
 		System.out.println("idŸ sobie do domu");
 		System.out.println("kurka na wacie");
 		System.out.println("Sworne gacie");
 		System.out.println(Arrays.toString(args));
-		System.out.println(args[0]);
+		if (args.length>0) {
+			System.out.println(args[0]);
+		}
 
 	
 }
