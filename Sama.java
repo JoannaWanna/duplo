f75a061311585d39d144587adfa8519bcd74718b
Joanna <joan-a@o2.pl>
2018-01-04 23:50:40 +0100

    Alez emocje

--- Sama.java
+++ Sama.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Sama {
 	public static void main(String[] args) {
 		System.out.println("Home alone");
+		System.out.println("Niby robiê tylko to co ju¿ umiem a to jest fajne");
 }
 
 }
