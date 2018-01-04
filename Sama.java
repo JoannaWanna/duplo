1ee4faeff5021a28909a2eea9540d27926bb6610
Joanna <joan-a@o2.pl>
2018-01-05 00:21:15 +0100

    sprawdzam czy dziala dodawanie

--- Sama.java
+++ Sama.java
@@ -4,6 +4,13 @@ public class Sama {
 	public static void main(String[] args) {
 		System.out.println("Home alone");
 		System.out.println("Niby robiê tylko to co ju¿ umiem a to jest fajne");
+		System.out.println(5+7);
+		System.out.println(Arrays.toString(args));
+		if (args.length>0) {
+			System.out.println(args[6]);
+		}
+		System.out.println(args.length);
+
 }
 
 }
