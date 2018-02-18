427b46c809ed8e93565f49b251516e902d37028c
Joanna <joan-a@o2.pl>
2018-02-18 20:40:26 +0100

    dodalam komentarz ze ide

--- Nepal.java
+++ Nepal.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Nepal {
 	public static void main(String[] args) {
 		System.out.println("Hello Nepal");
+		System.out.println("juz jest póŸno, zara idê");
 }
 
 }
